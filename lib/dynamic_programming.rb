ad7783002ddd21f64e82081339d30df065466626
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-01-10 09:08:12 -0800

    First commit

---
+++ lib/dynamic_programming.rb
@@ -0,0 +1,48 @@
+class DynamicProgramming
+  attr_accessor :cache
+
+  def initialize
+    @cache = []
+  end
+
+  def blair_nums(n)
+    # Base case(s): which inputs n should return early?
+    # Recursive case: what's the recursive relationship?
+    # *NB*: you'll need to figure out how to express the nth odd number in terms of n.
+    return 1 if n==1 
+    return 2 if n==2
+    blair_nums(n-2) + blair_nums(n-1) + 
+  end
+
+  def frog_hops_bottom_up(n)
+
+  end
+
+  def frog_cache_builder(n)
+
+  end
+
+  def frog_hops_top_down(n)
+
+  end
+
+  def frog_hops_top_down_helper(n)
+
+  end
+
+  def super_frog_hops(n, k)
+
+  end
+
+  def knapsack(weights, values, capacity)
+
+  end
+
+  # Helper method for bottom-up implementation
+  def knapsack_table(weights, values, capacity)
+
+  end
+
+  def maze_solver(maze, start_pos, end_pos)
+  end
+end
