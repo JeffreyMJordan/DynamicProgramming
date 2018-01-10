b407caa942754d33cbfd99b6fdf2acd74f514e48
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-01-10 09:43:02 -0800

    Completes frog_hops_bottom_up

--- lib/dynamic_programming.rb
+++ lib/dynamic_programming.rb
@@ -26,11 +26,24 @@ class DynamicProgramming
   end 
 
   def frog_hops_bottom_up(n)
-
+    cache = frog_cache_builder(n)
+    cache[n]
   end
 
   def frog_cache_builder(n)
+    # Make a hash or array for storing previous solutions
+    # Add the base cases to the cache  
+    # Build solutions 4..n into the cache
+    # Return the cache
 
+    cache = {}
+    cache[1] = [[1]]
+    cache[2] = [[1,1], [2]]
+    cache[3] = [[1,1,1], [1,2], [2,1], [3]]
+    (4..n).each do |idx| 
+      cache[idx] = cache[idx-1].deep_dup.map{|el| el << 1} + cache[idx-2].deep_dup.map{|el| el << 2} + cache[idx-3].deep_dup.map{|el| el << 3}
+    end
+    cache
   end
 
   def frog_hops_top_down(n)
@@ -57,3 +70,13 @@ class DynamicProgramming
   def maze_solver(maze, start_pos, end_pos)
   end
 end
+
+class Array 
+  def deep_dup
+    res = []
+    self.each do |el| 
+      res << el.dup 
+    end 
+    res 
+  end 
+end 
